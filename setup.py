a43cee2b3d5a0be50d414c517f091354da657915
Nikola Kovacevic <nikolak@outlook.com>
2014-10-27 20:00:48 +0100

    Added setup.py with py2exe support

---
+++ setup.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+try:
+    import py2exe
+except ImportError:
+    pass
+
+from RESTool.RESTool_main import __version__
+
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+requirements = [
+    # TODO: put package requirements here
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='RESTool',
+    version=__version__,
+    description='Unofficial cross platform tool for migrating between browsers, backing up, and restoring from backups, "Reddit Enhancement Suite" aka RES settings.',
+    long_description=readme + '\n\n' + history,
+    author='Nikola Kovacevic',
+    author_email='nikolak@outlook.com',
+    url='https://github.com/nikola-k/RESTool',
+    packages=[
+        'RESTool',
+    ],
+    package_dir={'RESTool':
+                 'RESTool'},
+    entry_points={
+        'console_scripts': [
+            'RESTool = RESTool.RESTool_main:main',
+        ],
+    },
+    console=['RESTool/RESTool_main.py'],
+    options={"py2exe":{
+            "dll_excludes": ["MSVCP90.dll", "HID.DLL", "w9xpopen.exe"],
+            'bundle_files': 1,
+            "includes":["sip"],
+            "compressed": 2,
+            "optimize":1,
+        }
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    license="Apache Software License",
+    zip_safe=False,
+    zipfile=None,
+    keywords='RESTool',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Intended Audience :: End Users/Desktop',
+        'Environment :: X11 Applications :: Qt',
+        'Topic :: Utilities',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
